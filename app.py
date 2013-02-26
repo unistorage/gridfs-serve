7289b1e4aad7a9dcc2040291432ba861d6a5624e
Anton Romanovich <anthony.romanovich@gmail.com>
2013-02-26 23:43:50 +0600

    Use ClosingIterator to properly handle connection

--- app.py
+++ app.py
@@ -1,86 +1,89 @@
 from email.utils import encode_rfc2231
 
 from gridfs import GridFS
-from werkzeug.wsgi import wrap_file
+from werkzeug.wsgi import wrap_file, ClosingIterator
 from werkzeug.wrappers import Request, Response
 from werkzeug.routing import Map, Rule
 from werkzeug.exceptions import abort, NotFound, HTTPException
 from unidecode import unidecode
 
 import settings
-from utils import ObjectIdConverter, LimitedFileWrapper, MongoDBConnection
+from utils import ObjectIdConverter, LimitedFileWrapper, get_mongodb_connection, MongoDBConnection
 
 
-def serve_full_file_request(request, headers, file):
+def serve_full_file_request(request, headers, file, callbacks=[]):
     headers.update({
         'Content-Length': file.length,
         'Accept-Ranges': 'bytes',
     })
-    response = Response(wrap_file(request.environ, file),
+    file_iterator = wrap_file(request.environ, file)
+    response = Response(ClosingIterator(file_iterator, callbacks=callbacks),
                         mimetype=file.content_type, headers=headers)
     response.last_modified = file.uploadDate
     response.set_etag(file.md5)
     return response
 
 
-def serve_partial_file_request(request, headers, file, start, end):
+def serve_partial_file_request(request, headers, file, start, end, callbacks=[]):
     # Note: byte positions are inclusive!
     headers.update({
         'Content-Length': end - start,
         'Content-Range': 'bytes %i-%i/%i' % (start, end - 1, file.length),
     })
-    return Response(LimitedFileWrapper(file, start, end),
+    file_iterator = LimitedFileWrapper(file, start, end)
+    return Response(ClosingIterator(file_iterator, callbacks=callbacks),
                     mimetype=file.content_type, headers=headers, status=206)
 
 
-def serve_request(request, _id=None):
-    with MongoDBConnection() as connection:
-        fs = GridFS(connection[settings.MONGO_DB_NAME])
-        try:
-            file = fs.get(_id)
-        except:
-            abort(404)
+def serve_request(request, connection, _id=None):
+    fs = GridFS(connection[settings.MONGO_DB_NAME])
+    try:
+        file = fs.get(_id)
+    except:
+        abort(404)
 
-        if request.if_modified_since:
-            if request.if_modified_since > file.uploadDate:
-                return Response(status=304)
-        if request.if_none_match:
-            if request.if_none_match.contains(file.md5):
-                return Response(status=304)
+    if request.if_modified_since:
+        if request.if_modified_since > file.uploadDate:
+            return Response(status=304)
+    if request.if_none_match:
+        if request.if_none_match.contains(file.md5):
+            return Response(status=304)
 
-        filename = file.filename
-        # Process non-latin filenames using technique described here:
-        # http://greenbytes.de/tech/tc2231/#encoding-2231-fb
-        rfc2231_filename = encode_rfc2231(filename.encode('utf-8'), 'UTF-8')
-        transliterated_filename = unidecode(filename)
-        content_disposition = 'inline; filename="%s"; filename*=%s;' % \
-                (transliterated_filename, rfc2231_filename)
+    filename = file.filename
+    # Process non-latin filenames using technique described here:
+    # http://greenbytes.de/tech/tc2231/#encoding-2231-fb
+    rfc2231_filename = encode_rfc2231(filename.encode('utf-8'), 'UTF-8')
+    transliterated_filename = unidecode(filename)
+    content_disposition = 'inline; filename="%s"; filename*=%s;' % \
+            (transliterated_filename, rfc2231_filename)
 
-        headers = {'Content-Disposition': content_disposition}
+    headers = {'Content-Disposition': content_disposition}
 
-        if not request.range:
-            return serve_full_file_request(request, headers, file)
-        else:
-            if request.range.units != 'bytes':
-                abort(400)
+    callbacks = [connection.close]
+    if not request.range:
+        return serve_full_file_request(request, headers, file, callbacks=callbacks)
 
-            ranges = request.range.ranges
-            if len(ranges) > 1:
-                return serve_full_file_request(request, headers, file)
+    if request.range.units != 'bytes':
+        abort(400)
 
-            start, end = ranges[0]
-            if end is None:
-                end = file.length
-            elif end > file.length:
-                abort(416)
+    ranges = request.range.ranges
+    if len(ranges) > 1:
+        return serve_full_file_request(request, headers, file, callbacks=callbacks)
 
-            return serve_partial_file_request(request, headers, file, start, end)
+    start, end = ranges[0]
+    if end is None:
+        end = file.length
+    elif end > file.length:
+        abort(416)
+
+    return serve_partial_file_request(request, headers, file, start, end,
+                                      callbacks=callbacks)
 
 
 url_map = Map([
-    Rule('/<ObjectId:_id>')
+    Rule('/<ObjectId:_id>'),
 ], converters={
-    'ObjectId': ObjectIdConverter
+    'ObjectId': ObjectIdConverter,
 })
 
 
@@ -89,9 +92,17 @@ def app(request):
     urls = url_map.bind_to_environ(request.environ)
     try:
         endpoint, args = urls.match()
-        return serve_request(request, **args)
     except HTTPException, e:
         return e
+    
+    connection = get_mongodb_connection()
+    try:
+        return serve_request(request, connection, **args)
+    except HTTPException, e:
+        return e
+    except:
+        connection.close()
+
 
 
 if __name__ == '__main__':
